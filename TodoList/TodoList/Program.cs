d9b73cdd031cc2d4c7216eaaee31ca1d2fc48c6c
aleksei <mican99900@gmail.com>
2025-09-21 15:25:28 +0300

    4 commit

--- TodoList/TodoList/Program.cs
+++ TodoList/TodoList/Program.cs
@@ -1,10 +1,19 @@
 ﻿using System.Globalization;
+namespace grib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Работу выполнили Нестеренко и Горелов");
+            Console.WriteLine("будьте любезны ввести свои фамилию и имя!");
+            string fullName = Console.ReadLine();
+            Console.WriteLine("Теперь введите дату рождения  в формате ДД.ММ.ГГГГ");
+            DateTime birthDate = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            DateTime currentDate = DateTime.Today;
+            int age = currentDate.Year - birthDate.Year;
+            Console.WriteLine("Добавлен пользователь " + fullName + ", " + "возраст - " + age + " лет");
+        }
+    }
+ } 
 
-Console.WriteLine("Работу выполнили Нестеренко и Горелов");
-Console.WriteLine("будьте любезны ввести свои фамилию и имя!");
-string fullName = Console.ReadLine();
-Console.WriteLine("Теперь введите дату рождения  в формате ДД.ММ.ГГГГ");
-DateTime birthDate = DateTime.ParseExact(Console.ReadLine(),"dd.MM.yyyy",CultureInfo.InvariantCulture);
-DateTime currentDate = DateTime.Today;
-int age = currentDate.Year - birthDate.Year;
-Console.WriteLine("Добавлен пользователь " + fullName + ", " + "возраст - " + age + " лет");
