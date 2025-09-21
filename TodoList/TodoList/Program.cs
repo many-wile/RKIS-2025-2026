0485aca228bc8f94f1c08b038d9dfa9cf3d1baa6
aleksei <mican99900@gmail.com>
2025-09-21 14:45:20 +0300

    shershen

---
+++ TodoList/TodoList/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Работу выполнили Нестеренко и Горелов");
+Console.WriteLine("будьте любезны ввести свои фамилию и имя!");
+string fullName = Console.ReadLine();
+Console.WriteLine("Теперь введите дату рождения  в формате ДД.ММ.ГГГГ");
+DateTime birthDate = DateTime.ParseExact(Console.ReadLine(),"dd.MM.yyyy",CultureInfo.InvariantCulture);
+DateTime currentDate = DateTime.Today;
+int age = currentDate.Year - birthDate.Year;
+Console.WriteLine("Добавлен пользователь " + fullName + ", " + "возраст - " + age + " лет");
