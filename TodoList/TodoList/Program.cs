be8e7872766d8d4249931ac8a14ee9414113f749
aleksei <mican99900@gmail.com>
2025-10-02 12:27:16 +0300

    lesson 3 make extension

--- TodoList/TodoList/Program.cs
+++ TodoList/TodoList/Program.cs
@@ -19,7 +19,7 @@ namespace grib
             DateTime currentDate = DateTime.Today;
             int age = currentDate.Year - birthDate.Year;
             Console.WriteLine("Добавлен пользователь " + fullName + ", " + "возраст - " + age + " лет");
-            string[] tasks = new string[10];
+            string[] tasks = new string[2];
             int taskCount = 0;
             string[] todos = { "help", "profile", "add", "view", "exit" };
             todos[1] = "profile - выводит данные пользователя";
@@ -59,6 +59,12 @@ namespace grib
                         }
                         else
                         {
+                            if (taskCount >=task.Length );
+                            {
+                                string[] newTask = new string[tasks.Length * 2];
+                                for (int i = 0; i < task.Length; i++);
+                                tasks = newTask;
+                            }
                             tasks[taskCount++] = parts[1];
                             Console.WriteLine("Задача добавлена: " + parts[1]);
                         }
