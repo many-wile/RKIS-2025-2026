6711df593c9485b7d153fea4cec076a99fc6d842
aleksei <mican99900@gmail.com>
2025-10-02 11:52:47 +0300

    lesson 3 make view, exit

--- TodoList/TodoList/Program.cs
+++ TodoList/TodoList/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.Design;
 using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
 using System.Runtime;
+using System.Threading.Tasks;
 namespace grib
 {
     internal class Program
@@ -17,6 +19,8 @@ namespace grib
             DateTime currentDate = DateTime.Today;
             int age = currentDate.Year - birthDate.Year;
             Console.WriteLine("Добавлен пользователь " + fullName + ", " + "возраст - " + age + " лет");
+            string[] tasks = new string[10];
+            int taskCount = 0;
             string[] todos = { "help", "profile", "add", "view", "exit" };
             todos[1] = "profile - выводит данные пользователя";
             todos[2] = "add - добавляет новую задачу";
@@ -42,13 +46,41 @@ namespace grib
                         break;
 
                     case "profile":
-                        Console.WriteLine(fullName + age);
+                        Console.WriteLine(fullName + " " + "Возраст: " + age);
                         break;
 
                     case "add":
+                        Console.WriteLine("введите команду");
                         string task = Console.ReadLine();
+                        string[] parts = task.Split(' ', 2);
+                        if (parts.Length < 2)
+                        {
+                            Console.WriteLine("Ошибка: используйте формат add текст задачи");
+                        }
+                        else
+                        {
+                            tasks[taskCount++] = parts[1];
+                            Console.WriteLine("Задача добавлена: " + parts[1]);
+                        }
                         break;
 
+
+                    case "view":
+                        Console.WriteLine("Ваши задачи:");
+                        for (int i = 0; i < taskCount; i++)
+                        {
+                            Console.WriteLine($"{i + 1}. {tasks[i]}");
+                        }
+                        if (taskCount == 0) Console.WriteLine("Нет задач.");
+                        break;
+                         
+                    case "exit":
+                        Console.WriteLine("Завершение задачи");
+                        return;
+
+                    default:
+                        Console.WriteLine("Неизвестная команда");
+                        break;
                 }
             }
         }
